eb5075fa6c72345353e98e0aa8f8d43ca022ef30
kalinith <kalinith@gmail.com.com>
2024-08-19 17:55:18 +0000

    refactor code

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 def main():
-	with open("books/frankenstein.txt") as f:
-		file_contents = f.read()
-
-	print(word_count(file_contents))
+	bookpath = "books/frankenstein.txt"
+	text = get_book_text(bookpath)
+	num_words = word_count(text)
+	print(f"{bookpath} contains {num_words} words.")
 
 
 def word_count(file_contents):
 	words = file_contents.split()
-	
 	return len(words)
 
 
-
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
 
 main()

fb8e76ca22d1b1b75b9919eb976f0cdc4e757979
kalinith <kalinith@gmail.com.com>
2024-08-19 18:24:35 +0000

    add function to get a count per letter

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ def main():
 	text = get_book_text(bookpath)
 	num_words = word_count(text)
 	print(f"{bookpath} contains {num_words} words.")
-
+	char_dict = char_count(text.lower())
+	print(char_dict)
 
 def word_count(file_contents):
 	words = file_contents.split()
@@ -13,5 +14,16 @@ def word_count(file_contents):
 def get_book_text(path):
     with open(path) as f:
         return f.read()
+
+def char_count(file_contents):
+	characters = {}
+	for i in range(0,len(file_contents)):
+		char = file_contents[i]
+		try:
+			count = characters[f"{char}"] + 1
+		except:
+			count = 0
+		characters.update({char: count})
+	return characters
 
 main()

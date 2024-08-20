13329932b492fe6b14c4722fbbc423ec59d458f0
kalinith <kalinith@gmail.com.com>
2024-08-20 04:54:33 +0000

    add function to sort by letter count and print report.

--- main.py
+++ main.py
@@ -2,9 +2,32 @@ def main():
 	bookpath = "books/frankenstein.txt"
 	text = get_book_text(bookpath)
 	num_words = word_count(text)
-	print(f"{bookpath} contains {num_words} words.")
 	char_dict = char_count(text.lower())
-	print(char_dict)
+	ordered_dict = dict_to_list(char_dict)
+	ordered_dict.sort(reverse=True, key=sort_on)
+
+	print(f"--- Begin report of {bookpath} ---")
+	print(f"{num_words} of words found in the document.")
+	print()
+	print()
+	print_wordcount(ordered_dict)
+	print("--- End report ---")
+
+
+def print_wordcount(characters):
+	for item in characters:
+		if item['char'].isalpha():
+			print(f"The {item['char']} character was found {item['num']} times")
+
+def dict_to_list(char_dict):
+	list_of_dicts = []
+	for char, num in char_dict.items():
+		list_of_dicts.append({"char":char, "num":num})
+	return list_of_dicts
+
+def sort_on(dict):
+    return dict["num"]
+
 
 def word_count(file_contents):
 	words = file_contents.split()
@@ -15,6 +38,7 @@ def get_book_text(path):
     with open(path) as f:
         return f.read()
 
+
 def char_count(file_contents):
 	characters = {}
 	for i in range(0,len(file_contents)):
@@ -22,7 +46,7 @@ def char_count(file_contents):
 		try:
 			count = characters[f"{char}"] + 1
 		except:
-			count = 0
+			count = 1
 		characters.update({char: count})
 	return characters
 
